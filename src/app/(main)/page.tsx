031ddecc031d0e0c16b4ea799aa88836dfd7843a
luozhiheng <651716183@qq.com>
2023-11-09 21:31:21 +0800

    main mod

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,4 +1,4 @@
-import { ProductListByCollectionDocument } from "@/gql/graphql";
+import { ProductListByCollectionDocument, type ProductListItemFragment } from "@/gql/graphql";
 import { executeGraphQL } from "@/lib/graphql";
 import { ProductList } from "@/ui/components/ProductList";
 
@@ -15,15 +15,14 @@ export default async function Page() {
 		},
 		revalidate: 60,
 	});
-
-	if (!data.collection?.products) throw Error("No products found");
-
-	const products = data.collection?.products.edges.map(({ node: product }) => product);
-
+	let products: readonly ProductListItemFragment[] = [];
+	if (data.collection?.products) {
+		products = data.collection?.products.edges.map(({ node: product }) => product);
+	}
 	return (
 		<div>
 			<section className="mx-auto max-w-7xl p-8 pb-16">
-				<h2 className="sr-only">Product list</h2>
+				<h2 className="sr-only">Product list </h2>
 				<ProductList products={products} />
 			</section>
 		</div>
